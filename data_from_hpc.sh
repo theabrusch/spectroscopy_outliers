02f395655c6e9406162eae160bb62042da8e1518
theb <s164238@student.dtu.dk>
2020-12-10 08:59:36 +0100

    data_from_hpc.sh

---
+++ data_from_hpc.sh
@@ -0,0 +1,1 @@
+rsync -av --rsh=ssh theb@login1.gbar.dtu.dk:/zhome/89/a/117273/Desktop/spectroscopy_outliers/outputs/ ~/Google\ Drive\ File\ Stream/Mit\ drev/Matematisk\ modellering/9.\ semester/Specialkursus/Code/spectroscopy_outliers/
